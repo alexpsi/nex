0501e93fd83e2c5d099fd2709edb5f933ba4c31f
Alex Psi <alexpsi256@gmail.com>
2017-08-12 16:50:50 +0300

    scratch

---
+++ index.js
@@ -0,0 +1,22 @@
+const program = require('commander');
+const Logger = require('winston');
+global._ = require('lodash');
+Logger.level = 'info';
+program.log = Logger;
+program.version(require('./package.json').version);
+
+// Top level program object exposes options to all subcommands
+program.
+  option('-h, --host [hostname]', 'Specify the transifex server', 'https://www.transifex.com').
+  option('-u, --user [username]', 'Specify the username').
+  option('-p, --pass [password]', 'Specify the password').
+  option('-t, --token [token]', 'Specify the transifex token').
+  option('-j, --json', 'Output JSON');
+
+program.on('--help', () => {
+  program.log.info(require('chalk').white.bold(`
+    EXPERIMENTAL BUILD
+  `));
+});
+
+module.exports = program;
